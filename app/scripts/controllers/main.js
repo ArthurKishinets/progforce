5b168644d9704aa10e8d989ca83e7a2efa9b9f91
ArthurKishinets <arthurkishinets@yahoo.com.ua>
2016-07-18 16:20:40 +0300

    first commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,36 +9,7 @@
  */
 angular.module('progforceApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.people = [
-      {
-      	'name': 'ivan',
-      	'surname': 'vasilyev',
-      	'email': 'google@com.ua',
-      	'phone': '0843513233234234',
-      	'birth': '02.04.1992'
-      },
-      {
-      	'name': 'igor',
-      	'surname': 'ivanov',
-      	'email': 'google@com.ua',
-      	'phone': '084352234234',
-      	'birth': '02.04.1992'
-      },
-      {
-      	'name': 'vanya',
-      	'surname': 'petrov',
-      	'email': 'google@com.ua',
-      	'phone': '084135234234',
-      	'birth': '02.04.1992'
-      },
-      {
-      	'name': 'dima',
-      	'surname': 'lasarev',
-      	'email': 'google@com.ua',
-      	'phone': '084315775234234',
-      	'birth': '02.04.1992'
-      }
-    ];
+    $scope.people = [];
 
     $scope.getWinner = function() {
     	var rand = Math.floor(Math.floor(Math.random() * (($scope.people.length - 1) - 0 + 1)));
